34f5546485475fd1ea4b673d591b64112d009cb2
leosquintero <leonardosquintero@gmail.com>
2019-08-13 12:52:39 -0400

    Initial commit

---
+++ startups/spiders/startup.py
@@ -0,0 +1,28 @@
+from scrapy import Spider
+from ..items import StartupsItem
+import scrapy
+import re 
+
+class StartupsSpider(Spider):
+    name = "startups"
+    allowed_domains = ['www.startupranking.com']
+    # Defining the list of pages to scrape
+    start_urls = ['https://www.startupranking.com/top'] + ['https://www.startupranking.com/top/0/' + str(1 * i)  for i in range(0, 3)]
+
+    def parse(self, response):
+        rows = response.xpath('//tr')
+        for row in rows:
+            name = row.xpath('.//td/div/a/text()').extract_first()
+            country = row.xpath('.//td/a/img/@title').extract_first()
+            
+            item = StartupsItem()
+            item['name'] = name
+            item['country'] = country
+            yield item
+
+        
+
+    def parse_detail(self, response):
+        item = response.meta['item']
+        link = response.xpath('/html/body/div[5]/section[1]/div[3]/h2/a')
+        yield item
